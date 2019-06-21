a2b35545357a87f11f1da0bb2751761068b49cb4
mtiburu <mtiburu@coe.neu.edu>
2019-06-21 17:05:15 -0400

    modified kNN

--- kNN.py
+++ kNN.py
@@ -1,3 +1,4 @@
+from os import listdir
 import numpy as np
 import operator
 
@@ -59,7 +60,52 @@ def datingClassTest():
 
 	for i in range(numTestVecs):
 		classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m,:], datingLabels[numTestVecs:m],3)
-		print('the class came back with: %d, the real answer is %d'.format(classifierResult, datingLabels[i]))
+		print('the class came back with: %d, the real answer is %d' % (classifierResult, datingLabels[i]))
 
 		if(classifierResult != datingLabels[i]): errorCount += 1.0
-		print('the total error rate is: %f'.format(errorCount/float(numTestVecs)))
+	print('the total error rate is: %f' % (errorCount/float(numTestVecs)))
+
+def classifyPerson():
+
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	persentTats = float(raw_input('percentage of time spent playing games? '))
+	ffMiles = float(raw_input('frquent flier miles earned /year? '))
+	iceCream = float(raw_input('liters of ice cream consumed /year? '))
+	datingDataMat, datingLabels = fileToMatrix('datingTestSet.txt')
+	normMat, ranges, minVals = normalize(datingDataMat)
+	inArr = np.array([ffMiles, persentTats, iceCream])
+	classifierResult = classify0((inArr-minVals) / ranges, normMat, datingLabels, 3)
+	print('You\'ll probably like this person: ', resultList[classifierResult - 1])
+
+def imgToVec(filename):
+	returnVect = np.zeros((1,1024))
+	fileread = open(filename)
+	for i in range(32):
+		lineStr = fileread.readline()
+		for j in range(32):
+			returnVect[0, 32*i+j] = int(lineStr[j])
+	return returnVect
+def handwritingClassify0():
+	hwLabels = []
+	trainingFileList = listdir('trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = np.zeros((m, 1024))
+	for i in range(m):
+		fileNameStr = trainingFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i, :] = imgToVec('trainingDigits/%s' % fileNameStr)
+		testFileList = listdir('testDigits')
+		errorCount = 0.0
+		mTest = len(testFileList)
+		for i in range(mTest):
+			fileNameStr = testFileList[i]
+			fileStr = fileNameStr.split('.')[0]
+			classNumStr = int(fileStr.split('_')[0])
+			vectorUnderTest = imgToVec('testDigits/%s' % fileNameStr)
+			classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+			print('classifier with: %d, real answer: %d' % (classifierResult, classNumStr))
+			if(classifierResult != classNumStr): errorCount += 1.0
+		print('total number of errors: %d' % errorCount)
+		print('error rate: %f' % (errorCount/float(mTest)))
